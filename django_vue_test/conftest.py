18ca6793c14db4e73d05b6b7b275c4440b7b7497
Jochen Wersdörfer <jochen@wersdoerfer.de>
2020-04-26 15:37:14 +0200

    initial checkin

---
+++ django_vue_test/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from django_vue_test.users.models import User
+from django_vue_test.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
