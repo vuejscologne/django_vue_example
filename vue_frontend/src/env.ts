301eeb775b9ca394fe5396419f590b988a359649
Jochen Wersdörfer <jochen@wersdoerfer.de>
2020-05-06 17:34:22 +0200

    use django backend

--- vue_frontend/src/env.ts
+++ vue_frontend/src/env.ts
@@ -2,12 +2,13 @@ const env = process.env.VUE_APP_ENV;
 
 let envApiUrl = '';
 
+console.log('env is set to: ', env);
 if (env === 'production') {
   envApiUrl = `https://${process.env.VUE_APP_DOMAIN_PROD}`;
 } else if (env === 'staging') {
   envApiUrl = `https://${process.env.VUE_APP_DOMAIN_STAG}`;
 } else {
-  envApiUrl = `http://${process.env.VUE_APP_DOMAIN_DEV}`;
+  envApiUrl = `http://${process.env.VUE_APP_DOMAIN_DEV}:8000`;
 }
 
 export const apiUrl = envApiUrl;
