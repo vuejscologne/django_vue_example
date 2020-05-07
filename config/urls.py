b24d0ad50295993b172214cbd415851070fcfde8
Jochen Wersdörfer <jochen@wersdoerfer.de>
2020-05-07 18:43:51 +0200

    consistent name with fastapi

--- config/urls.py
+++ config/urls.py
@@ -41,12 +41,20 @@ urlpatterns += [
     ),
     # swagger
     path(
-        "swagger-ui/",
+        "docs/",
         TemplateView.as_view(
             template_name="swagger-ui.html",
             extra_context={"schema_url": "openapi-schema"},
         ),
         name="swagger-ui",
+    ),
+    # redoc
+    path(
+        "redoc/",
+        TemplateView.as_view(
+            template_name="redoc.html", extra_context={"schema_url": "openapi-schema"}
+        ),
+        name="redoc",
     ),
 ]
 
