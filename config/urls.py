c198e7a30a5c83ebaad03367dd64c4004fe1dcac
Jochen Wersdörfer <jochen@wersdoerfer.de>
2020-05-07 18:21:39 +0200

    better name+

--- config/urls.py
+++ config/urls.py
@@ -35,7 +35,7 @@ urlpatterns += [
     path(
         "openapi",
         get_schema_view(
-            title="Your Project", description="API for all things …", version="1.0.0"
+            title="testapi", description="API for all things …", version="1.0.0"
         ),
         name="openapi-schema",
     ),
