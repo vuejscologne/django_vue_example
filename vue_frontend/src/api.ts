d71886f01ed1c346e01c1dbb1606a9c1018f8dcc
Jochen Wersdörfer <jochen@wersdoerfer.de>
2020-06-04 15:54:52 +0200

    move api functions into class

--- vue_frontend/src/api.ts
+++ vue_frontend/src/api.ts
@@ -1,14 +1,15 @@
-import axios from "axios";
-import { apiUrl } from "@/env";
-import router from "./router";
+import axios from 'axios';
+import { apiUrl } from '@/env';
+import router from './router';
 import {
   IUserProfile,
   IUserProfileUpdate,
-  IUserProfileCreate
-} from "./interfaces";
+  IUserProfileCreate,
+} from './interfaces';
 
-const ACCESS_TOKEN_KEY = "access_token";
-const REFRESH_TOKEN_KEY = "refresh_token";
+const TOKEN_PREFIX = 'test_api';
+const ACCESS_TOKEN_KEY = `${TOKEN_PREFIX}_access_token`;
+const REFRESH_TOKEN_KEY = `${TOKEN_PREFIX}_refresh_token`;
 
 function authHeaderFromLocalStorage() {
   const token = localStorage.getItem(ACCESS_TOKEN_KEY);
@@ -28,62 +29,64 @@ function addTokensToLocalStorage(token) {
 
 function getConfig() {
   const config = {
-    baseURL: "http://localhost:8000/api/"
+    baseURL: apiUrl,
   };
   const headers = authHeaderFromLocalStorage();
   if (headers) {
-    config["headers"] = headers;
+    config['headers'] = headers;
   }
   return config;
 }
 
-const instance = axios.create(getConfig());
+// const instance = axios.create(getConfig());
 
+/*
 export const api = {
   async logInGetToken(username: string, password: string) {
     const params = new URLSearchParams();
-    params.append("username", username);
-    params.append("password", password);
+    params.append('username', username);
+    params.append('password', password);
 
     return axios.post(`${apiUrl}/api/token/`, params);
   },
   setToken(token) {
-    console.log("set token: ", token);
+    console.log('set token: ', token);
     addTokensToLocalStorage(token);
     instance.defaults.headers = authHeaderFromLocalStorage();
   },
   loggedIn() {
-    console.log("loggedIn? ", instance.defaults.headers);
-    return "Authorization" in instance.defaults.headers;
+    console.log('loggedIn? ', instance.defaults.headers);
+    return 'Authorization' in instance.defaults.headers;
   },
   async getMe() {
-    return axios.get<IUserProfile>("users/me/");
+    return axios.get<IUserProfile>('users/me/');
   },
   async updateMe(data: IUserProfileUpdate) {
-    return axios.put<IUserProfile>("users/me/", data);
+    return axios.put<IUserProfile>('users/me/', data);
   },
   async getUsers() {
-    return axios.get<IUserProfile[]>("users/");
+    return axios.get<IUserProfile[]>('users/');
   },
   async updateUser(userId: number, data: IUserProfileUpdate) {
-    return axios.put("users/${userId}/", data);
+    return axios.put('users/${userId}/', data);
   },
   async createUser(data: IUserProfileCreate) {
-    return axios.post("users/", data);
+    return axios.post('users/', data);
   },
   async passwordRecovery(email: string) {
-    return axios.post("password-recovery/${email}");
+    return axios.post('password-recovery/${email}');
   },
   async resetPassword(password: string) {
-    return axios.post("reset-password/", {
+    return axios.post('reset-password/', {
       // prettier-ignore
       new_password: password
     });
-  }
+  },
 };
+*/
 
 export class ApiClient {
-  instance: any;
+  private instance: any;
 
   constructor() {
     const instance = axios.create(getConfig());
@@ -116,29 +119,72 @@ export class ApiClient {
     );
     */
     this.instance = instance;
-    console.log("in constructor: ");
+    console.log('in constructor: ');
   }
 
-  async logInGetToken(username: string, password: string) {
+  public async logInGetToken(username: string, password: string) {
     const payload = { username, password };
-    return this.instance.post("token/", payload);
+    return this.instance.post('token/', payload);
   }
 
-  authHeaderFromLocalStorage() {
+  public loggedIn() {
+    console.log('loggedIn? ', this.instance.defaults.headers);
+    return 'Authorization' in this.instance.defaults.headers;
+  }
+
+  public setToken(token) {
+    console.log('set token: ', token);
+    addTokensToLocalStorage(token);
+    this.instance.defaults.headers = {
+      ...this.instance.defaults.headers,
+      ...this.authHeaderFromLocalStorage(),
+    };
+  }
+
+  public logOut() {
+    console.log('api logout..');
+    removeTokensFromLocalStorage();
+  }
+
+  public async getMe() {
+    return this.instance.get('users/me/');
+  }
+
+  public async updateMe(data: IUserProfileUpdate) {
+    return this.instance.put('users/me/', data);
+  }
+
+  public async getUsers() {
+    return this.instance.get('users/');
+  }
+
+  public async updateUser(userId: number, data: IUserProfileUpdate) {
+    return this.instance.put('users/${userId}/', data);
+  }
+
+  public async createUser(data: IUserProfileCreate) {
+    return this.instance.post('users/', data);
+  }
+
+  public async passwordRecovery(email: string) {
+    return this.instance.post('password-recovery/${email}');
+  }
+
+  public async resetPassword(password: string) {
+    return this.instance.post('reset-password/', {
+      // prettier-ignore
+      new_password: password,
+    });
+  }
+
+  private authHeaderFromLocalStorage() {
     const token = localStorage.getItem(ACCESS_TOKEN_KEY);
     if (!token) return null;
     return { Authorization: `Bearer ${token}` };
   }
-
-  setToken(token) {
-    console.log("set token: ", token);
-    addTokensToLocalStorage(token);
-    this.instance.defaults.headers = {
-      ...this.instance.defaults.headers,
-      ...this.authHeaderFromLocalStorage()
-    };
-  }
 }
+
+export const api = new ApiClient();
 /*
 instance.interceptors.response.use(
   (response) => {
